1521655346b580b18293181eda0cb722c468912e
Brian McClelland <47439126+bmcclelland@users.noreply.github.com>
2019-03-04 20:54:19 -0800

    Added Util class

---
+++ Sockets/Util.cs
@@ -0,0 +1,64 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Sockets
+{
+    class Util
+    {
+        public static string SocketLocalIP(Socket socket)
+        {
+            IPEndPoint ep = socket.LocalEndPoint as IPEndPoint;
+            
+            if (ep == null)
+            {
+                return "unknown";
+            }
+            else
+            {
+                return ep.Address.MapToIPv4().ToString();
+            }
+        }
+
+        public static string SocketRemoteIP(Socket socket)
+        {
+            IPEndPoint ep = socket.RemoteEndPoint as IPEndPoint;
+
+            if (ep == null)
+            {
+                return "unknown";
+            }
+            else
+            {
+                return ep.Address.MapToIPv4().ToString();
+            }
+        }
+
+        public static string SocketLocalPort(Socket socket)
+        {
+            var ep = socket.LocalEndPoint as IPEndPoint;
+
+            if (ep == null)
+            {
+                return "unknown";
+            }
+            else
+            {
+                return ep.Port.ToString();
+            }
+        }
+
+        public static string SocketRemotePort(Socket socket)
+        {
+            var ep = socket.RemoteEndPoint as IPEndPoint;
+
+            if (ep == null)
+            {
+                return "unknown";
+            }
+            else
+            {
+                return ep.Port.ToString();
+            }
+        }
+    }
+}
