4210427b6a9936942fec75a399b7988b8b4e9277
Joshua Arco <joshuarco@gmail.com>
2019-03-04 19:02:16 -0800

    Adding ui

--- Sockets/Program.cs
+++ Sockets/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace Sockets
+﻿using System;
+
+namespace Sockets
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(args.Length);
+            foreach(string arg in args)
+                Console.WriteLine(arg);
+
             Server app = new Server(8000);
+
+            Console.WriteLine("Listening on " /*+ IPAddress.Any.ToString()*/ + ":" + app.LocalPort);
+
+            string line = "";
+            while ((line = Console.ReadLine()) != null)
+            {
+                Console.WriteLine("");
+
+                switch (line)
+                {
+                    case "help":
+                        Console.WriteLine("Option " + line);
+                        break;
+                    case "myip":
+                        Console.WriteLine("Option " + line);
+                        break;
+                    case "myport":
+                        Console.WriteLine("Option " + app.LocalPort);
+                        break;
+                    default:
+                        Console.WriteLine("You dun scrued up...");
+                        break;
+                }
+            }
         }
     }
 }
--- Sockets/Server.cs
+++ Sockets/Server.cs
@@ -8,11 +8,12 @@ namespace Sockets
     {
         PeerManager peers = new PeerManager();
         Socket listenSocket;
-        int localPort;
+        public int LocalPort { get; private set; }
+        public string serverIP { get { return IPAddress.Any.ToString(); } }
         
         public Server(int port)
         {
-            localPort = port;
+            LocalPort = port;
             Run();
         }
 
@@ -22,54 +23,16 @@ namespace Sockets
             Console.WriteLine("onIncomingConnect " + peerSocket.RemoteEndPoint.ToString());
             peers.AddPeer(peerSocket);
             Console.WriteLine("added peer " + peerSocket.RemoteEndPoint.ToString());
-        } 
+        }
 
         void Run()
         {
-            var localEndPoint = new IPEndPoint(IPAddress.Any, localPort);
+            var localEndPoint = new IPEndPoint(IPAddress.Any, LocalPort);
             listenSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
             listenSocket.Bind(localEndPoint);
             listenSocket.Listen(10);
             var callback = new AsyncCallback(OnIncomingConnect);
             listenSocket.BeginAccept(callback, listenSocket);
-            Console.WriteLine("Listening");
-
-            while (true)
-            {}
         }
     }
 }
-
-/*
-private static Socket ConnectSocket(string server, int port)
-{
-    Socket s = null;
-    IPHostEntry hostEntry = null;
-
-    // Get host related information.
-    hostEntry = Dns.GetHostEntry(server);
-
-    // Loop through the AddressList to obtain the supported AddressFamily. This is to avoid
-    // an exception that occurs when the host IP Address is not compatible with the address family
-    // (typical in the IPv6 case).
-    foreach (IPAddress address in hostEntry.AddressList)
-    {
-        IPEndPoint ipe = new IPEndPoint(address, port);
-        Socket tempSocket =
-            new Socket(ipe.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
-        tempSocket.Connect(ipe);
-
-        if (tempSocket.Connected)
-        {
-            s = tempSocket;
-            break;
-        }
-        else
-        {
-            continue;
-        }
-    }
-    return s;
-}
-*/
